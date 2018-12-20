5e22bb468f930e8db0b89d0890384a9ca59afc23
Ahmed Diab <dibooo30@gmail.com>
2018-12-20 04:02:05 +0200

    add first push to github

---
+++ main.js
@@ -0,0 +1,23 @@
+"use strict";
+import './components/projects/headar';
+// var shoesShop1 = document.getElementById('shoesShop1');
+// var shoesShop1Span = document.getElementById('shoesShop1Span');
+
+// var shoesShop2 = document.getElementById('shoesShop2');
+// var overlay    = document.getElementById('overlay');
+// shoesShop1.addEventListener('click', move);
+// shoesShop2.addEventListener('click', move);
+// var t = shoesShop1.firstElementChild;
+
+// function move(){
+//     event.target.classList.add('d-fixed');
+//     overlay.classList.add('d-block');
+// }
+// shoesShop1Span.addEventListener('click', moveBack())
+
+// function moveBack(){
+//     console.log(event);
+    
+//     // event.target.parentElement.classList.remove('d-fixed');
+//     overlay.classList.remove('d-block');
+// }
